3dc69d312f86302e88ff8a87cf4cd5e7b9643bce
FabianTUW <e11776167@student.tuwien.ac.at>
2024-02-19 20:17:03 +0100

    fixup! add pico_probe as an adapter for rpi pico

--- pico_probe.py
+++ pico_probe.py
@@ -3,12 +3,6 @@
 SIM Modem Script ... Runs on our measurement nodes in controlled environment on Raspberry Pi
 """
 
-# venv hack (to make it work with sudo):
-import os
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-activate_this = os.path.join(base_dir, "mobileatlas/probe/venv/bin/activate_this.py")
-exec(open(activate_this).read(), {"__file__": activate_this})
 
 import hashlib
 import serial, ssl, socket, struct
@@ -25,6 +19,7 @@ from smartcard.util import toHexString
 from functools import reduce
 from itertools import zip_longest
 from datetime import date
+import signal
 
 
 logger = logging.getLogger(__name__)
@@ -32,15 +27,20 @@ logger = logging.getLogger(__name__)
 SOCKET_RECV_LENGTH = 4096  # 2**12
 DELAY_RELAY_SECONDS = 1.2
 
+LOOP = True
+
+def signal_handler(sig, frame):
+    global LOOP
+    LOOP = False
 
 def wait_for_pico() -> serial.Serial:
-    while True:
+    while LOOP:
         try:
             s: serial.Serial = serial.Serial(
                 port="/dev/ttyACM0",
                 parity=serial.PARITY_EVEN,
                 stopbits=serial.STOPBITS_ONE,
-                timeout=0.01,
+                timeout=1.1,
             )
             return s
         except Exception as e:
@@ -54,80 +54,93 @@ def relay_with_pico(imsi, connection: ssl.SSLSocket, delay_relay=False):
     _atr = None
     while _atr is None or len(_atr) == 0:
         _atr = connection.recv(33)  # ATR max length
-    atr = bytearray(b"\x20")
-    atr += bytearray(len(_atr).to_bytes(2, "big"))
-
-    logging.info("ATR, %i: %s", len(atr), _atr)
-    atr += bytearray(
-        reduce(lambda x, y: x + y, map(lambda y: y.to_bytes(1, "big"), _atr))
-    )
-
+    atr = _atr
     s = wait_for_pico()
-    logging.info("pico -> atr")
-    while True:
-        msg = s.read_until("\n")
-        if len(msg) == 0:
-            s.write(b"\x10\x00\x00")
-            time.sleep(1)
-        elif b"Waiting for " in msg:
-            break
-        else:
-            logging.info(msg)
-
-    s.write(atr)
-    s.flush()
-    logging.info("atr exchanged")
+    logging.info("pico connected")
     pico_measurements = []
     probe_measurements = []
 
-    while True:
-        header: bytes = s.read(2)
+    OP_APDU = 0x00
+    OP_RESET = 0x01
+    OP_DEBUGMSG = 0x02
+    OP_SENDATR = 0x03
+    OP_MEASUREMENT = 0x04
+    OP_REQUEST_STATE = 0x05
+
+    buf = struct.pack("=BI", OP_REQUEST_STATE, 0)
+    s.write(buf)
+
+    while LOOP:
+        header = s.read(5)
         if len(header) == 0:
             continue
-        if header[0] == 16:  # b'\x10':
-            length: int = int.from_bytes(header[1:3], byteorder="big", signed=True)
-            body = s.read(length)
-            if len(body) < 5:
-                continue
+        opcode = header[0]
+        length, = struct.unpack("I", header[1:])
+        data = s.read(length)
+        if opcode == OP_APDU:
             start_probe = time.perf_counter_ns()
-
-            connection.send(body)
-            response = connection.recv(SOCKET_RECV_LENGTH)
-            comm = bytearray(b"\x10")
-            comm += bytearray(len(response).to_bytes(2, "big"))
-
-            if delay_relay:
-                logging.debug("sleep %fs", DELAY_RELAY_SECONDS)
-                time.sleep(DELAY_RELAY_SECONDS)
-            s.write(comm + bytearray(response))
-            s.flush()
+            connection.send(data)
+            resp = connection.recv(65535)
             end_probe = time.perf_counter_ns()
-            logging.info("command %s | response %s", toHexString(list(body)), toHexString(list(response)))
+
+            buf = struct.pack("=BI", OP_APDU, len(resp))
+            logging.info(f"Forwarding header {buf} (len {len(resp)})")
+            s.write(buf)
+            s.write(resp)
             probe_measurements.append(
                 (
-                    end_probe - start_probe,
-                    toHexString(list(body)),
-                    toHexString(list(response)),
+                    (end_probe - start_probe) // 1000,
+                    toHexString(list(data)),
+                    toHexString(list(resp)),
                 )
             )
-        else:
-            body = s.read_until(b"\n")
-            try:
-                line = (header + body).decode()
-                if "Waiting for " in line:
-                    continue
-                logging.info(line)
-                if line.startswith("done"):
-                    logging.info("stop relaying: %s", line)
-                    return pico_measurements, probe_measurements
-                if line.startswith("diff "):
-                    splits = line.split("=", maxsplit=1)
-                    logging.info(splits[1])
-                    ms = splits[1].split(",")
-                    logging.info(ms)
-                    pico_measurements.append(list(map(lambda x: int(x), ms)))
-            except UnicodeDecodeError:
-                print((header + body))
+            logging.info(f"Forwarding response {len(resp)} {toHexString(list(resp))}")
+        elif opcode == OP_RESET:
+            pass
+        elif opcode == OP_DEBUGMSG:
+            msg = data#.decode('ascii')
+            logging.info(f"Debug: {msg}")
+        elif opcode == OP_SENDATR: #or (opcode == OP_REQUEST_STATE and data == b'1'):
+            buf = struct.pack("=BI", OP_SENDATR, len(atr))
+            s.write(buf)
+            s.write(atr)
+        elif opcode == OP_REQUEST_STATE:
+            if data == b'\x00':
+                buf = struct.pack("=BI", OP_SENDATR, len(atr))
+                s.write(buf)
+                s.write(atr)
+        elif opcode == OP_MEASUREMENT:
+            msg = data.decode('ascii')
+            splits = msg.split(',')
+            pico_measurements.append(list(map(int, splits)))
+
+    return pico_measurements, probe_measurements
+
+def format_measurement(x):
+    probe, pico = x
+    if len(pico) == 0:
+        return ",".join(
+            [
+                datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                probe[0],  # time diff probe
+                '-1',  # time diff pico E2E
+                '-1',  # time diff pico read command to send command to proxy
+                '-1',  # time diff pico read command to receive response
+                probe[1],
+                probe[2],
+            ]
+        )
+    return ",".join(
+        [
+            datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+            probe[0],  # time diff probe
+            str(pico[0]),  # time diff pico E2E
+            str(pico[1]),  # time diff pico read command to send command to proxy
+            str(pico[2]),  # time diff pico read command to receive response
+            probe[1],
+            probe[2],
+        ]
+    )
 
 
 def main():
@@ -145,12 +158,6 @@ def main():
     except ValueError as e:
         exit(f"{e}\nExiting...")
 
-    if not parser.get_direct_tunnel():
-        try:
-            api_token = Token(base64.b64decode(os.environ["API_TOKEN"]))
-        except:
-            exit("API_TOKEN environment variable is unset.\nExiting...")
-
     # Create modem tunnel
     logger.info("setup modem tunnel...")
 
@@ -161,39 +168,32 @@ def main():
     tls_ctx.verify_mode = ssl.CERT_NONE
     logger.info(f"tls context {tls_ctx.verify_mode}")
 
-    reader_name = "HID Global OMNIKEY 3x21 Smart Card Reader [OMNIKEY 3x21 Smart Card Reader] 00 00".strip()
-    canonical_name = reader_name[:-5]
-    imsi = abs(int(hashlib.md5(canonical_name.encode()).hexdigest(), 16)) % (2 ** 64)
-    imsi = 232077613951364
-    logging.info(f"reader with canonical name '{canonical_name}' and imsi {imsi} added")
+    if parser.get_use_reader():
+        canonical_name = parser.get_reader_name()[:-5]
+        imsi = abs(int(hashlib.md5(canonical_name.encode()).hexdigest(), 16)) % (2 ** 64)
+    else:
+        imsi = parser.get_imsi()
 
+    logger.info(f"requesting {imsi}")
     socket_connection = tls_ctx.wrap_socket(
         socket.create_connection((parser.get_host(), parser.get_port())),
         server_hostname=parser.get_tls_server_name(),
     )
-    logger.info(f"requesting {imsi}")
-    while True:
-        pico_measurement, probe_measurement = relay_with_pico(imsi, socket_connection, delay_relay=False)
 
-        print(pico_measurement)
+    while LOOP:
+        pico_measurements, probe_measurements = relay_with_pico(
+            imsi, socket_connection, delay_relay=False
+        )
         measurements = reduce(
             lambda x, y: x + "\n" + y,
             map(
-                lambda x: ",".join(
-                    [
-                        datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                        x[0][0],
-                        x[1],
-                        x[0][1],
-                        x[0][2],
-                    ]
-                ),
+                format_measurement,
                 zip_longest(
                     (
                         (str(a), '"' + str(b) + '"', '"' + str(c) + '"')
-                        for (a, b, c) in probe_measurement
+                        for (a, b, c) in probe_measurements
                     ),
-                    map(str, pico_measurement),
+                    pico_measurements,
                     fillvalue="",
                 ),
             ),
@@ -204,9 +204,10 @@ def main():
 
         with open("measurements.csv", mode="a", encoding="utf-8") as measurement_file:
             measurement_file.write(measurements + "\n")
-        socket_connection.close()
+
     exit(0)
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     main()
